eacdda3d36657b1ce039a4d117a5e847a82c1c48
Jose Manuel Adrian Iramain <jiramain@todo1.net>
2021-04-21 16:42:57 -0300

    delet footer, add get wallet by user token

--- src/components/User/Wallets.jsx
+++ src/components/User/Wallets.jsx
@@ -7,13 +7,23 @@ import "./UserList.css";
 
 export default function Wallet({ darkMode }) {
   const {user} = useContext(userContext),
-  [wallets, setWallets] = useState([]);
+  [wallets, setWallets] = useState([]),
+  [total, setTotal] = useState('-');
 
   useEffect(() => {
     if (user && user.token) {
+      axios({
+        method: "GET",
+        url: "user",
+        headers: { "x-access-token": user.token}
+      }).then(({data}) => {
+        setTotal(data.send_user.total.total_1);
+        setWallets(data.send_user.total.data);
+      }).catch(e => console.log('error :>> ', e));
       // obtener wallets
     } else setWallets([]);
   }, [user]);
+
   return (
     <div>
       <Row justify="center" align="center">
@@ -24,81 +34,44 @@ export default function Wallet({ darkMode }) {
           </p>
         </Col>
         <Col xs={{ span: 8 }}>
-          <p>3.7553356120719528e+22 </p>
+          <p>ETH {total}</p>
         </Col>
       </Row>
       <Row justify="center" align="center">
-        <Col xs={{ span: 18 }} lg={{ span: 6 }} style={{ marginTop: "40px" }}>
-          <Card>
-            <Row>
-              <Col xs={{ span: 12 }}>
-                <p>
-                  <DollarCircleOutlined style={{ marginRight: "5px" }} />
-                  Moneda
-                </p>
-              </Col>
-              <Col xs={{ span: 12 }}>
-                <p>Eht </p>
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={{ span: 12 }}>
-                <p>
-                  <DollarCircleOutlined style={{ marginRight: "5px" }} />
-                  Balance
-                </p>
-              </Col>
-              <Col xs={{ span: 12 }}>
-                <p>57099056771678952 </p>
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={{ span: 12 }}>
-                <p>Total</p>
-              </Col>
-              <Col xs={{ span: 12 }}>
-                <p> 375533561207195283087</p>
-              </Col>
-            </Row>
-          </Card>
+      {
+        wallets.length > 0 ?
+        <Col xs={{ span: 18 }} lg={{ span: 6 }}>
+          {wallets.map(w =>
+            <Card style={{ marginTop: "40px" }} key={w._id}>
+              <Row>
+                <Col xs={{ span: 12 }}>
+                  <p>Moneda</p>
+                </Col>
+                <Col xs={{ span: 12 }}>
+                  <p>ETH</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={{ span: 12 }}>
+                  <p>Cuenta</p>
+                </Col>
+                <Col xs={{ span: 12 }}>
+                  <p>{w.account}</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={{ span: 12 }}>
+                  <p><DollarCircleOutlined style={{ marginRight: "5px" }} />Total</p>
+                </Col>
+                <Col xs={{ span: 12 }}>
+                  <p>${w.balance}</p>
+                </Col>
+              </Row>
+            </Card>
+          )} 
         </Col>
-        <Col lg={{ span: 24 }}></Col>
-        <Col xs={{ span: 18 }} lg={{ span: 6 }} style={{ marginTop: "40px" }}>
-          <Card>
-            <Row>
-              <Col xs={{ span: 12 }}>
-                <p>
-                  <DollarCircleOutlined style={{ marginRight: "5px" }} />
-                  Moneda
-                </p>
-              </Col>
-              <Col xs={{ span: 12 }}>
-                <p>Eht </p>
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={{ span: 12 }}>
-                <p>
-                  <DollarCircleOutlined style={{ marginRight: "5px" }} />
-                  Balance
-                </p>
-              </Col>
-              <Col xs={{ span: 12 }}>
-                <p>57099056771678952 </p>
-              </Col>
-            </Row>
-            <Row>
-              <Col xs={{ span: 12 }}>
-                <p>57099056771678952</p>
-              </Col>
-              <Col xs={{ span: 12 }}>
-                <p> precio total</p>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
-        <Col lg={{ span: 24 }}></Col>
-        
+        : <h3>No se encontraron billeteras</h3>
+      }
       </Row>
     </div>
   );
